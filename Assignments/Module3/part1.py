eadb2f3bfa09b1fc1fab419076352343470d3cb3
Shreyash <ptlsh4292@gmail.com>
2026-01-25 22:43:47 -0500

    adding comments ans screenshort

--- Assignments/Module3/part1.py
+++ Assignments/Module3/part1.py
@@ -1,3 +1,10 @@
+# Write a program that calculates the total amount of a meal 
+# purchased at a restaurant. The program should ask the user 
+# to enter the charge for the food and then calculate the 
+# amounts with an 18 percent tip and 7 percent sales tax. 
+# Display each of these amounts and the total price.
+
+
 #constant
 TIP_RATE=0.18
 TAX_RATE=0.07
