266da662c93de84da478b45428ea758de7536e4a
Shreyash <ptlsh4292@gmail.com>
2026-01-25 22:14:05 -0500

    Updation location for assignments

---
+++ Assignments/Module 1/Assignment/Module1/part1.py
@@ -0,0 +1,11 @@
+## User Input and Basic Arithmetic Operations
+user_inpute1=int(input("Enter first number: "))
+user_inpute2=int(input("Enter second number: "))
+
+## Addition
+addition=user_inpute1+user_inpute2
+print("The sum of",user_inpute1,"and",user_inpute2,"is",addition)
+
+## Subtraction
+subtraction=user_inpute1-user_inpute2
+print("The difference when",user_inpute2,"is subtracted from",user_inpute1,"is",subtraction)
